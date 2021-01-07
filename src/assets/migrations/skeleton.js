40cc9f60993a6d70f5d680062d8656120b911fec
Rami kass youssef <rami.k.youssef@gmail.com>
2021-01-07 13:28:24 +0200

    Updated migration skeleton

--- src/assets/migrations/skeleton.js
+++ src/assets/migrations/skeleton.js
@@ -1,21 +1,32 @@
 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('sequelize')} Sequelize
+ */
+const up = async (queryInterface, Sequelize) => {
+	/**
+	 * Add altering commands here.
+	 *
+	 * Example:
+	 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+	 */
+};
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('sequelize')} Sequelize
+ */
+const down = async (queryInterface, Sequelize) => {
+	/**
+	 * Add reverting commands here.
+	 *
+	 * Example:
+	 * await queryInterface.dropTable('users');
+	 */
+};
+
+module.exports = {
+	up,
+	down,
 };
