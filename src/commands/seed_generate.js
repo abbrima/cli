35ebb8c091fffe0e78235d05eb7e464641fa3b81
Rami kass youssef <rami.k.youssef@gmail.com>
2021-01-18 05:53:56 +0200

    updated sequelize-cli

--- src/commands/seed_generate.js
+++ src/commands/seed_generate.js
@@ -4,20 +4,38 @@ import helpers from '../helpers';
 import fs from 'fs';
 import clc from 'cli-color';
 
+const roles = ['sys'];
+
 exports.builder = (yargs) =>
-  _baseOptions(yargs).option('name', {
-    describe: 'Defines the name of the seed',
-    type: 'string',
-    demandOption: true,
-  }).argv;
+  _baseOptions(yargs)
+    .option('name', {
+      describe: 'Defines the name of the seed',
+      type: 'string',
+      demandOption: true,
+    })
+    .option('role', {
+      describe: 'create a seeder for modifying a role',
+      type: 'string',
+      choices: roles,
+      demandOption: false,
+    }).argv;
 
 exports.handler = function (args) {
   helpers.init.createSeedersFolder();
 
+  let filePath = 'seeders/skeleton.js';
+  if (args.role) {
+    if (!roles.includes(args.role))
+      throw new Error(
+        `Invalid role provided, allowed are [${roles.join(',')}]`
+      );
+    if (roles == 'sys') filePath = 'seeders/sysRoleSkeleton.js';
+  }
+
   fs.writeFileSync(
     helpers.path.getSeederPath(args.name),
     helpers.template.render(
-      'seeders/skeleton.js',
+      filePath,
       {},
       {
         beautify: false,
@@ -26,7 +44,7 @@ exports.handler = function (args) {
   );
 
   helpers.view.log(
-    'New seed was created at',
+    'New role seed was created at',
     clc.blueBright(helpers.path.getSeederPath(args.name)),
     '.'
   );
